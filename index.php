254c6fb35a4b6e525bf581a9ba2fbbdca396b1c3
PetrBuslyuk <skyliner270594@mail.ru>
2016-01-08 00:15:02 +0200

    styles (in process)

--- index.php
+++ index.php
@@ -7,32 +7,44 @@ $mas2 = $connection->query('SELECT title,cost,img_url,create_data FROM ads');
     echo $row['img_url'] . "<br>";
 }*/
 ?>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-<div class="headers">
-    <a href="registration.php">Register</a>
-    <a href="add_ad.php"> Add an advert</a>
-    <a href="">Go to profile</a>
-</div>
-<div class="objavleniya">
-    <h2>Ads</h2>
-    <?php
-    while ($row = $mas2->fetch()) {
-        echo "<div class='obyavlenie'>";
-        $temp = $row['img_url'];
-        echo "<img src='fotos/" . $temp . "'>";
-        echo "<a class='link' href=''>" . $row['title'] . "</a>";
-        echo "<div class='create_data'>" . $row['create_data'] . "</div>";
-        echo $row['cost'] . "$";
-        echo "</div>";
-        echo "<br/>";
-    }
-    ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-5 ">
+            <header>
+                <a href="registration.php">Register</a>
+                <a href="add_ad.php"> Add an advert</a>
+                <a href="">Go to profile</a>
+            </header>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row"><h2>Ads</h2></div>
+            <div class="row">
+            <?php
+            while ($row = $mas2->fetch()) {
+                echo "<div class='obyavlenie'>";
+                $temp = $row['img_url'];
+                echo "<img src='fotos/" . $temp . "'>";
+                echo "<a class='link' href=''>" . $row['title'] . "</a>";
+                echo "<div class='create_data'>" . $row['create_data'] . "</div>";
+                echo $row['cost'] . "$";
+                echo "</div>";
+                echo "<br/>";
+            }
+            ?>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
