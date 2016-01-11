27c1a64d21ec3ddf9021125fcd564f860996a9d4
kyakimchuk <kyakimchuk95@mail.ru>
2016-01-11 22:01:24 +0200

    changed profile.php and functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,14 @@
 <?php
+
 class funcs
 {
     function user_exist($login, $password)
     {
-        $hashed_pass=md5($password);
+        $hashed_pass = md5($password);
         $connection = new PDO('mysql:host=localhost; port=65535; dbname=doskaobyavl', 'root', '');
         $result = $connection->query("SELECT id_member FROM memberlist where login='$login' and pass='$hashed_pass'");
         //var_dump ($result->fetchAll());
-        $user_inf=$result->fetchAll();
+        $user_inf = $result->fetchAll();
         if (count($user_inf) == 1)
             return $user_inf[0]['id_member'];
         else return false;
@@ -18,33 +19,46 @@ class funcs
         session_start();
         $_SESSION["user_id"] = $user_id;
     }
+
     function get_ad($id_ad)
     {
         $connection = new PDO('mysql:host=localhost; port=65535; dbname=doskaobyavl', 'root', '');
         $result = $connection->query("SELECT * FROM ads where id_ad='$id_ad'")->fetch();
-        $categ_id=$result["category_id"];
-        $type_id=$result["type_id"];
-        $categ=$connection->query("SELECT name FROM categories where id_category='$categ_id'")->fetchColumn();
-        $type=$connection->query("SELECT name FROM types where id_type='$type_id'")->fetchColumn();
-        $result["categ"]=$categ;
-        $result["type"]=$type;
+        $categ_id = $result["category_id"];
+        $type_id = $result["type_id"];
+        $categ = $connection->query("SELECT name FROM categories where id_category='$categ_id'")->fetchColumn();
+        $type = $connection->query("SELECT name FROM types where id_type='$type_id'")->fetchColumn();
+        $result["categ"] = $categ;
+        $result["type"] = $type;
         return $result;
     }
+
     function get_user($id_user)
     {
         $connection = new PDO('mysql:host=localhost; port=65535; dbname=doskaobyavl', 'root', '');
         $result = $connection->query("SELECT * FROM memberlist where id_member='$id_user'")->fetch();
         return $result;
     }
-    function get_type($id_type){
+
+    function get_type($id_type)
+    {
         $connection = new PDO('mysql:host=localhost; port=65535; dbname=doskaobyavl', 'root', '');
-        $type=$connection->query("SELECT name FROM types where id_type='$id_type'")->fetchColumn();
+        $type = $connection->query("SELECT name FROM types where id_type='$id_type'")->fetchColumn();
         return $type;
     }
-    function get_categ($id_category){
+
+    function get_categ($id_category)
+    {
         $connection = new PDO('mysql:host=localhost; port=65535; dbname=doskaobyavl', 'root', '');
-        $categ=$connection->query("SELECT name FROM categories where id_category='$id_category'")->fetchColumn();
+        $categ = $connection->query("SELECT name FROM categories where id_category='$id_category'")->fetchColumn();
         return $categ;
+    }
+
+    function get_user_ads($user_email)
+    {
+        $connection = new PDO('mysql:host=localhost; port=65535; dbname=doskaobyavl', 'root', '');
+        $result = $connection->query("SELECT * FROM ads where email='$user_email'")->fetchAll();
+        return $result;
     }
 }
 
--- profile.php
+++ profile.php
@@ -2,24 +2,45 @@
 require_once "functions.php";
 session_start();
 $functs = new funcs();
-$user_id=$_SESSION["user_id"];
+$user_id = $_SESSION["user_id"];
 $user_inf = $functs->get_user($user_id);
+$user_ads = $functs->get_user_ads($user_inf['email']);
+$ads_count = count($user_ads);
 ?>
 <html>
 <head>
     <meta charset="utf-8">
 </head>
 <body>
-<!--
-<h3><?php echo $user_inf['title'];?></h3>
-<h5>Type: <?php echo $ad_inf['type'];?></h5>
-<h5>Category: <?php echo $ad_inf['categ'];?></h5>
-<p><?php echo $ad_inf['create_data'];?></p>
-<p><?php echo $ad_inf['cost']."$";?></p>
-<p><?php echo $ad_inf['email'];?></p>
-<img src='<?php echo "photos/".$ad_inf["img"];?>'>
-<p><?php echo $ad_inf['message'];?></p>
--->
+<h2>My information</h2>
+
+<p>Login: <?php echo $user_inf['login']; ?></p>
+
+<p>Name: <?php echo $user_inf['name']; ?></p>
+
+<p>Surname: <?php echo $user_inf['surname']; ?></p>
+
+<p>Email: <?php echo $user_inf['email']; ?></p>
+<h2>My ads</h2>
+<?php
+for ($i = 0; $i < $ads_count; $i++) {
+    $categ = $functs->get_categ($user_ads[$i]['category_id']);
+    $type = $functs->get_type($user_ads[$i]['type_id']);
+    echo "<div class='obyavlenie'>";
+    $temp = $user_ads[$i]['img'];
+    echo "<img style='width:150px' src='photos/" . $temp . "'>";
+    echo "<a class='link' href='advert.php?id=" . $user_ads[$i]['id_ad'] . "'>" . $user_ads[$i]['title'] . "</a>";
+    echo "<h5>Type: " . $type . "</h5>";
+    echo "<h5>Category: " . $categ . "</h5>";
+    echo "<div class='create_data'>" . $user_ads[$i]['create_data'] . "</div>";
+    echo $user_ads[$i]['cost'] . "$";
+    echo "</div>";
+    echo "<br/>";
+}?>
+<?php if ($ads_count==0) :?>
+    <p>You don't have ads now</p>
+    <a href="add_ad.php"><span class="glyphicon glyphicon-pencil"></span>Add an advert</a>
+<?php endif;?>
 </body>
 </html>
 
