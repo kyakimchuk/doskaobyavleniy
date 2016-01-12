23271cd4316a94f7cd7ac5cdea2c9e447796feb8
PetrBuslyuk <skyliner270594@mail.ru>
2016-01-12 20:12:39 +0200

    styles advert

--- advert.php
+++ advert.php
@@ -13,16 +13,19 @@ $ad_inf = $functs->get_ad($id_ad);
     <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-<a href="index.php">Home</a>
+<div class="container">
+    <br>
+<a href="index.php"><h4>HOME</h4></a><br><br><br>
+    <img class="imageAdvert" src='<?php echo "photos/".$ad_inf["img"];?>'>
 <h3><?php echo $ad_inf['title'];?></h3>
 <h5>Type: <?php echo $ad_inf['type'];?></h5>
 <h5>Category: <?php echo $ad_inf['categ'];?></h5>
-<p><?php echo $ad_inf['create_data'];?></p>
-<p><?php echo $ad_inf['cost']."$";?></p>
-<p><?php echo $ad_inf['email'];?></p>
-<img src='<?php echo "photos/".$ad_inf["img"];?>'>
-<p><?php echo $ad_inf['message'];?></p>
+<p>Date of creation: <?php echo $ad_inf['create_data'];?></p>
+<p>Price: <?php echo $ad_inf['cost']."$";?></p>
+<p>Owner email: <?php echo $ad_inf['email'];?></p>
 
+<p>Description: <?php echo $ad_inf['message'];?></p>
+</div>
 </body>
 </html>
 
